cb1becd90bba15ea91ebc9e3741a733ad9df28f2
abi perez <abiperez@abis-macbook-air.local>
2018-03-19 14:37:14 -0600

    Initial Commit

---
+++ primo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  primo
+//
+//  Created by abi perez on 3/19/18.
+//  Copyright © 2018 abi perez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
